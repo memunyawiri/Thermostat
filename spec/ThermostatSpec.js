2e5734bbfeda2b5e2005b6129f4175052acaf008
Mutsa <mutsa.evelyn@hotmail.com>
2017-08-04 13:15:26 +0100

    Added jQuery to index.html

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -2,12 +2,6 @@
 
 describe('Thermostat', function() {
 
-  var thermostat;
-
-  beforeEach(function() {
-    thermostat = new Thermostat();
-  });
-
   it('starts at 20 degrees', function() {
     expect(thermostat.getCurrentTemperature()).toEqual(20);
   });

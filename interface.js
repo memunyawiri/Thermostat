c1bb311cd36365e66891d5acd12eca54dbe2f954
Mutsa <mutsa.evelyn@hotmail.com>
2017-08-04 15:37:22 +0100

    Implemented world temp with weather API

--- interface.js
+++ interface.js
@@ -29,6 +29,13 @@ $(document).ready(function() {
     updateTemperature();
   });
 
+  $('#current-city').change(function() {
+  var city = $('#current-city').val();
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=5179136c2531a43420fd64958fec4323&units=metric', function(data) {
+    $('#current-temperature').text(data.main.temp)
+  })
+})
+
   function updateTemperature() {
     $('#temperature').text(thermostat.getCurrentTemperature());
     $('#temperature').attr('class', thermostat.energyUsage());
